3a2a1d3b4a7532626e2d81d0ac0dd454ff7d1309
ignacioRosales <nachorosales2604@gmail.com>
2023-09-29 22:33:39 -0300

    Arreglo del alert

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-alert("Algoritmo de verificar edad")
+alert("Algoritmo de verificar edad para entrar a una discoteca")
 
 
 let edad = parseInt(prompt("Ingrese su edad: "))
@@ -16,7 +16,7 @@ while(edad != 0 && edad < 100 && edad !== "" && edad !== null){
         alert("Usted puede ingresar a la discoteca pero no puede comprar bebidas alcoholicas")
 
     }else{ 
-            alert("Usted puede ingresar a la discoteca y puede comprar bebidas alcoholicas")
+        alert("Usted puede ingresar a la discoteca y puede comprar bebidas alcoholicas")
     }
 
     edad = parseInt(prompt("Siguiente edad en la fila: "))
