c3d8c95d8a0fcb243c1b9f245cbdb61532f3db8c
ignacioRosales <nachorosales2604@gmail.com>
2023-09-29 22:11:36 -0300

    Arregle el algoritmocon sus condicionales correctos

--- script.js
+++ script.js
@@ -5,19 +5,27 @@ alert("Algoritmo de verificar edad")
 let edad = parseInt(prompt("Ingrese su edad: "))
 
 
-while( edad != 0 && edad < 100) {
+while(edad != 0 && edad < 100 && edad !== "" && edad !== null){
 
     if (edad < 18 ) {
+
         alert("Usted no puede ingresar")
+
     }else if( edad >= 18  && edad <= 21) {
+
         alert("Usted puede ingresar a la discoteca pero no puede comprar bebidas alcoholicas")
-    }else { 
-        alert("Usted puede ingresar a la discoteca y puede comprar bebidas alcoholicas")
+
+    }else{ 
+            alert("Usted puede ingresar a la discoteca y puede comprar bebidas alcoholicas")
     }
 
-    edad = parseInt(prompt("Ingrese otra edad: "))
-    break
+    edad = parseInt(prompt("Siguiente edad en la fila: "))
+
+   
 }
+
+
+
 
 
 
